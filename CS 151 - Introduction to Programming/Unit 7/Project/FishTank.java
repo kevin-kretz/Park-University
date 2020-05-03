674aaf6b5afe101fe3ce8fc531df7c45a57c5ffa
Kevin Kretz <kkretz17@gmail.com>
2020-05-03 17:01:20 -0600

    Added gallonCapacity and numGoldfish

--- CS 151 - Introduction to Programming/Unit 7/Project/FishTank.java
+++ CS 151 - Introduction to Programming/Unit 7/Project/FishTank.java
@@ -2,11 +2,16 @@ public class FishTank {
   private int lengthInInches;
   private int widthInInches;
   private int heightInInches;
+  private int numGoldfish;
+  private int gallonCapacity;
+  private static int totalGoldfish;
 
   public FishTank(int lengthInInches, int widthInInches, int heightInInches) {
     this.lengthInInches = lengthInInches;
     this.widthInInches = widthInInches;
     this.heightInInches = heightInInches;
+    this.gallonCapacity = (this.lengthInInches * this.widthInInches * this.heightInInches) / 231;
+    this.numGoldfish = 0;
   }
 
 }
