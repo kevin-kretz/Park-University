d584a14e2c0c3405354f4db1a98bfe5f7e6af67d
Kevin Kretz <kkretz17@gmail.com>
2020-05-03 16:59:44 -0600

    Created constructor for FishTank

---
+++ CS 151 - Introduction to Programming/Unit 7/Project/FishTank.java
@@ -0,0 +1,12 @@
+public class FishTank {
+  private int lengthInInches;
+  private int widthInInches;
+  private int heightInInches;
+
+  public FishTank(int lengthInInches, int widthInInches, int heightInInches) {
+    this.lengthInInches = lengthInInches;
+    this.widthInInches = widthInInches;
+    this.heightInInches = heightInInches;
+  }
+
+}
