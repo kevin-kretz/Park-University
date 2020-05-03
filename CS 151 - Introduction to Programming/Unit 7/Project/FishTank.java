d043d62d6aad15b7c7b8c81d8461dc4aaff389f2
Kevin Kretz <kkretz17@gmail.com>
2020-05-03 17:08:27 -0600

    Added displayTankInformation() method
    -created isBigEnough() method to simplify the code inside displayTankInformation()
    -corrected data types from int to double for some variables

--- CS 151 - Introduction to Programming/Unit 7/Project/FishTank.java
+++ CS 151 - Introduction to Programming/Unit 7/Project/FishTank.java
@@ -1,30 +1,66 @@
 public class FishTank {
-  private int lengthInInches;
-  private int widthInInches;
-  private int heightInInches;
+  private double lengthInInches;
+  private double widthInInches;
+  private double heightInInches;
   private int numGoldfish;
-  private int gallonCapacity;
+  private double gallonCapacity;
   private static int totalGoldfish;
-
-  public FishTank(int lengthInInches, int widthInInches, int heightInInches) {
+ 
+  public FishTank(double lengthInInches, double widthInInches, double heightInInches) {
     this.lengthInInches = lengthInInches;
     this.widthInInches = widthInInches;
     this.heightInInches = heightInInches;
     this.gallonCapacity = (this.lengthInInches * this.widthInInches * this.heightInInches) / 231;
     this.numGoldfish = 0;
   }
-
-  public void addGoldfish(){
+ 
+  public void addGoldfish() {
     this.numGoldfish++;
     totalGoldfish++;
   }
-
+ 
   public void removeGoldfish() {
     this.numGoldfish--;
     totalGoldfish--;
   }
-
+ 
   public void transferGoldfish(FishTank newFishTank) {
     newFishTank.numGoldfish += this.numGoldfish;
     this.numGoldfish = 0;
+  }
+ 
+  // created for use in displayTankInformation()
+  public boolean isBigEnough() {
+    double gallonsNeeded;
+ 
+    if (numGoldfish > 1) {
+      gallonsNeeded = ((numGoldfish - 1) * 12) + 30;
+    }
+    else if (numGoldfish == 1) {
+      gallonsNeeded = 30;
+    }
+    else {
+      gallonsNeeded = 0;
+    }
+ 
+    if (gallonCapacity >= gallonsNeeded) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+ 
+  public void displayTankInformation() {
+    System.out.printf("This tank is %,.1f inches long, %,.1f inches wide, and %,.1f inches tall.\n", lengthInInches,
+        widthInInches, heightInInches);
+    System.out.printf("It can hold up to %,.1f gallons of water.\n", gallonCapacity);
+    System.out.println("Currently, it holds " + this.numGoldfish + " goldfish.");
+    if (this.isBigEnough()) {
+      System.out.println("This tank is sufficiently large for its current number of goldfish.");
+    }
+    else {
+      System.out.println("This tank is too small for its current number of goldfish.");
+    }
+  }
 }
