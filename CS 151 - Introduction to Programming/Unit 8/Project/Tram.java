4e0c90baaf6656a3446d498025204deca411fb2a
Kevin Kretz <kkretz17@gmail.com>
2020-05-10 19:40:21 -0600

    Added comments

--- CS 151 - Introduction to Programming/Unit 8/Project/Tram.java
+++ CS 151 - Introduction to Programming/Unit 8/Project/Tram.java
@@ -10,8 +10,8 @@ public class Tram {
 
   // Constructor
   public Tram(String name, int capacity) {
-    this.name = name;
-    this.capacity = capacity;
+    this.name = name; // sets tram's name
+    this.capacity = capacity; // sets tram's capacity
   }
 
   // sellChildTicket method
@@ -19,7 +19,7 @@ public class Tram {
     double ticketCost = 4.50; // sets ticket cost
     System.out.println("A child wants to buy a ticket.");
     this.addPerson(age, ticketCost); // passes ticket to helper method
-    return this;
+    return this; // allows method chaining
   }
 
   // sellAdultTicket
@@ -27,26 +27,34 @@ public class Tram {
     double ticketCost = 8.50; // sets ticket cost
     System.out.println("An adult wants to buy a ticket.");
     this.addPerson(age, ticketCost); // passes ticket to helper method
-    return this;
+    return this; // allows method chaining
   }
 
   // addPerson helper method
   public void addPerson(int age, double ticketCost) {
+    // if the train is not full
     if (this.purchasedSeats < this.capacity) {
+      // if the child is under 3, they don't take up a seat and get to ride for free.
       if (age <= 3) {
         System.out.println("The child is young enough to share a seat, so they ride for free.");
-      } else if (age >= 65) {
-        ticketCost -= 2.00;
-        ticketEarnings += ticketCost;
-        purchasedSeats++;
+      }
+      // if the adult is over 65, they get $2 off their ticket price
+      else if (age >= 65) {
+        ticketCost -= 2.00; // reduces ticket price
+        ticketEarnings += ticketCost; // add ticketCost to ticketEarnings
+        purchasedSeats++; // increases purchasedSeats by 1
         System.out.printf("The adult is old enough to receive the senior discount, so they pay only $%,.2f.\n",
             ticketCost);
-      } else {
-        purchasedSeats++;
-        ticketEarnings += ticketCost;
+      }
+      // if not younger than 3 or older than 65
+      else {
+        purchasedSeats++; // increases purchasedSeats by 1
+        ticketEarnings += ticketCost; // add ticketCost to ticketEarnings
         System.out.printf("A ticket has been sold for $%,.2f.\n", ticketCost);
       }
-    } else {
+    }
+    // if the train is full
+    else {
       System.out.println("Sorry, the tram is full. Unable to purchase ticket.");
     }
   }
