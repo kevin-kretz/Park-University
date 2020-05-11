2269544a441576938fddf2de102893045728599d
Kevin Kretz <kkretz17@gmail.com>
2020-05-10 19:16:29 -0600

    added class to file.

--- CS 151 - Introduction to Programming/Unit 8/Project/TramDriver.java
+++ CS 151 - Introduction to Programming/Unit 8/Project/TramDriver.java
@@ -1,25 +1,25 @@
 /**********************************************
- * Kevin Kretz - 10 May 2020
- * This is the driver class for a tram ticket
- * purchasing system.  The driver was provided
- * as a part of project instructions.
+ * Kevin Kretz - 10 May 2020 This is the driver class for a tram ticket
+ * purchasing system. The driver was provided as a part of project instructions.
  **********************************************/
-public static void main(String[] args) {
-  Tram tram1 = new Tram("Orient Express", 30);
-  Tram tram2 = new Tram("African Safari", 5);
+public class TramDriver {
+  public static void main(String[] args) {
+    Tram tram1 = new Tram("Orient Express", 30);
+    Tram tram2 = new Tram("African Safari", 5);
 
-  // Sell some tickets for tram #1
-  System.out.println("Tram #1");
-  tram1.sellAdultTicket(49).sellAdultTicket(30).sellAdultTicket(65);
-  tram1.sellChildTicket(12).sellChildTicket(3);
-  System.out.println();
-  tram1.displaySeatsAndEarnings();
+    // Sell some tickets for tram #1
+    System.out.println("Tram #1");
+    tram1.sellAdultTicket(49).sellAdultTicket(30).sellAdultTicket(65);
+    tram1.sellChildTicket(12).sellChildTicket(3);
+    System.out.println();
+    tram1.displaySeatsAndEarnings();
 
-  // Sell some tickets for tram #2
-  System.out.println("\nTram #2");
-  for (int i=0; i<7; i++)   {
-    tram2.sellAdultTicket(30);
-  }
-  System.out.println();
-  tram2.displaySeatsAndEarnings();
-} // end main
+    // Sell some tickets for tram #2
+    System.out.println("\nTram #2");
+    for (int i = 0; i < 7; i++) {
+      tram2.sellAdultTicket(30);
+    }
+    System.out.println();
+    tram2.displaySeatsAndEarnings();
+  } // end main
+} // end class
