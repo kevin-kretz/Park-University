d7225bcb76c76cada5c894dae935b10abb8458b4
Kevin Kretz <kkretz17@gmail.com>
2020-05-10 18:42:53 -0600

    Added constructor

---
+++ CS 151 - Introduction to Programming/Unit 8/Project/Tram.java
@@ -0,0 +1,14 @@
+/**********************************************
+ * Kevin Kretz - 10 May 2020 This is the Tram class for a tram ticket purchasing
+ * system. The driver was provided as a part of project instructions.
+ **********************************************/
+public class Tram {
+  private String name;
+  private int capacity;
+
+  // Constructor
+  public Tram(String name, int capacity) {
+    this.name = name;
+    this.capacity = capacity;
+  }
+}
