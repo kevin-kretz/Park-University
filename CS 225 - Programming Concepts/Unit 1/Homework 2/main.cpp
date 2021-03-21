d36021dff2215c46a95ace740c8d44274055773c
Kevin Kretz <kkretz17@gmail.com>
2021-03-21 07:08:35 -0800

    Create main.cpp
    So far the program gathers the user's entered phone number and stores it in a variable.

---
+++ CS 225 - Programming Concepts/Unit 1/Homework 2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  // Included input and output streams
+#include <fstream>  // Include file streams
+#include <string> // Include string
+#include <iomanip>  // Include input output manipulation
+
+int main() {
+  // Gather user input
+  std::string userPhoneNumber;
+  std::cout << "Enter a telephone number: ";
+  std::cin >> userPhoneNumber;
+  std::cout << userPhoneNumber;
+
+
+  // Return 0 and end of program 
+  return 0;   
+}
