a1c2ac3eda018225f43bcd21d9ec539560215e40
Kevin Kretz <kkretz17@gmail.com>
2021-03-23 08:22:43 -0800

    Create HW3_Kretz.cpp

---
+++ CS 225 - Programming Concepts/Unit 2/HW3_Kretz.cpp
@@ -0,0 +1,10 @@
+// This program converts the time from 24-hour notation to 12-hour notation and vice versa.
+// Unit 2 - Homework 3
+// Kevin Kretz | 23 March 2021
+
+#include <iostream>
+
+int main() {
+
+    return 0;
+}
