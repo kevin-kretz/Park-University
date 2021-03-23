b8b31074a43cee4b70e734464475fd6927e5e309
Kevin Kretz <kkretz17@gmail.com>
2021-03-23 10:38:56 -0800

    Added get time function
    gets the time from the user and saves it as a string in variable time

--- CS 225 - Programming Concepts/Unit 2/HW3_Kretz.cpp
+++ CS 225 - Programming Concepts/Unit 2/HW3_Kretz.cpp
@@ -4,23 +4,43 @@
 
 #include <iostream>
 
-void showMenu() {
+int displayMenu() {
+    int menuChoice;
+    
     std::cout << "Options --" << std::endl;
     std::cout << "1: To convert time from 12-hour notation to 24-hour notation." << std::endl;
     std::cout << "2: To convert time from 24-hour notation to 12-hour notation." << std::endl;
     std::cout << "0: To quit the program." << std::endl;
     std::cout << "Choose: ";
+    std::cin >> menuChoice;
+    std::cout << "\n";
+    return menuChoice;
 }
 
-int getUserChoice() {
-    int userChoice;
-    showMenu();
-    std::cin >> userChoice;
-    return userChoice;
+std::string getTime(int menuChoice){
+    std::string hours;
+    std::string minutes;
+    std::string amPM;
+    
+    std::cout << "Enter hours: ";
+    std::cin >> hours;
+    std::cout << "Enter minutes: ";
+    std::cin >> minutes;
+    
+    if (menuChoice == 1) {
+        std::cout << "Enter AM/PM (A or P): ";
+        std::cin >> amPM;
+        return hours + " " + minutes + " " + amPM;
+    }
+    else {
+        return hours + " " + minutes;
+    }
+    
 }
 
 int main() {
-    int userChoice = getUserChoice();
-    std::cout << userChoice;
+    int menuChoice = displayMenu();
+    std::string time = getTime(menuChoice);
+    std::cout << time;
     return 0;
 }
