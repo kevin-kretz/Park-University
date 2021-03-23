e9fd949c2a1cbf02f24c80cb804924a07008aeec
Kevin Kretz <kkretz17@gmail.com>
2021-03-23 08:34:55 -0800

    Created menu and gets user choice
    Created a menu for the user to choose which conversion they want
    Accepts user input and stores in a variable

--- CS 225 - Programming Concepts/Unit 2/HW3_Kretz.cpp
+++ CS 225 - Programming Concepts/Unit 2/HW3_Kretz.cpp
@@ -4,7 +4,23 @@
 
 #include <iostream>
 
-int main() {
+void showMenu() {
+    std::cout << "Options --" << std::endl;
+    std::cout << "1: To convert time from 12-hour notation to 24-hour notation." << std::endl;
+    std::cout << "2: To convert time from 24-hour notation to 12-hour notation." << std::endl;
+    std::cout << "0: To quit the program." << std::endl;
+    std::cout << "Choose: ";
+}
 
+int getUserChoice() {
+    int userChoice;
+    showMenu();
+    std::cin >> userChoice;
+    return userChoice;
+}
+
+int main() {
+    int userChoice = getUserChoice();
+    std::cout << userChoice;
     return 0;
 }
